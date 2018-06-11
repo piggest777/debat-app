fd5ee148e635a0cabde564670b0d41206059a5ed
Ксения Калмыкова <monro_8@mail.ru>
2018-06-11 18:54:01 +0300

    start create

---
+++ debat-app/SetUpTeamVC.swift
@@ -0,0 +1,21 @@
+//
+//  SetUpTeamVC.swift
+//  debat-app
+//
+//  Created by Denis Rakitin on 11/06/2018.
+//  Copyright © 2018 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class SetUpTeamVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
--- debat-app/StartDebateVC.swift
+++ debat-app/StartDebateVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartDenateVC.swift
 //  debat-app
 //
 //  Created by Denis Rakitin on 11/06/2018.
@@ -8,17 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartDebateVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func startGameBtnWasPressed(_ sender: Any) {
     }
+    
+    @IBOutlet weak var teamSetBtnWasPressed: UIButton!
 
 
 }
